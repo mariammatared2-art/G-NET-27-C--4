256c871b9687505ccd04f2f2047bcb4e8149436b
mariam matared <mariammatared2@gmail.com>
2026-02-06 11:33:13 +0200

    Question 2

--- Program.cs
+++ Program.cs
@@ -41,6 +41,28 @@ namespace ConsoleApp11
             //Console.WriteLine("StringBuilder time: " + sw.ElapsedMilliseconds + " ms");
             #endregion
 
+            #region Question 2
+            //Console.Write("Enter age: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("Enter day of week (1–7): ");
+            //int day = int.Parse(Console.ReadLine());
+            //Console.Write("Do you have a student ID? (yes/no): ");
+            //bool isStudent = Console.ReadLine().ToLower() == "yes";
+            //double price = 0;
+            //if (age < 5)
+            //    price = 0;
+            //else if (age <= 12)
+            //    price = 30;
+            //else if (age <= 59)
+            //    price = 50;
+            //else
+            //    price = 25;
+            //if (price > 0 && (day == 6 || day == 7))
+            //    price += 10;
+            //if (price > 0 && isStudent)
+            //    price *= 0.8;
+            //Console.WriteLine("Final Ticket Price: " + price + " EGP");
+            #endregion
         }
     }
 }
